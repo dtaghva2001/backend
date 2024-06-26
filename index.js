0639725e3a5b4063ebc67590073ae847414105aa
dtaghva2001 <135502648+dtaghva2001@users.noreply.github.com>
2024-06-26 09:16:26 +0330

    new feature backend

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express'
 import myRouter from './myRouter.js'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import {connect_database} from "./config/connect_database.js";
-
+dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(myRouter)
-connect_database()
 
+connect_database()
 const PORT = 1235;
 
 app.listen(PORT, () => {
--- myRouter.js
+++ myRouter.js
@@ -2,19 +2,12 @@ import express from "express";
 import {SignUpAndLoginController} from "./controller/SignUpAndLoginController.js";
 import {BlogController} from "./controller/BlogController.js";
 import {ModelGeneratorController} from "./controller/ModelGeneratorController.js";
-
+import {APIGeneratorController} from "./controller/APIGeneratorController.js";
 const myRouter = express.Router()
 myRouter.post('/sign-up', SignUpAndLoginController.sign_up)
 myRouter.post('/login', SignUpAndLoginController.login)
-
-//todo these are the APIs that I'm not sure about
-// router.post('/add_to_blog', BlogController.addPost)
-// router.put('/edit_blog_post', BlogController.editPost)
-// router.delete('/delete_post', BlogController.deletePost)
-// router.get('/all_blogs', BlogController.checkAllPosts)
-// router.post('/review_post', BlogController.reviewPost)
-
-myRouter.post('/generate_model_express', ModelGeneratorController.generateExpressModel)
-myRouter.post('/generate_model_django', ModelGeneratorController.generateDjangoModel)
-// myRouter.post('/generate_model_php', ModelGeneratorController.generatePHPModel)
+myRouter.post('/generate_model_express', ModelGeneratorController.generateExpressModel);
+myRouter.post('/generate_model_django', ModelGeneratorController.generateDjangoModel);
+myRouter.post('/generate_api', APIGeneratorController.generateGPT3Response);
+myRouter.post('/download1', ModelGeneratorController.downloadModel)
 export default myRouter;
